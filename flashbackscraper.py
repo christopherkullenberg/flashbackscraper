17ae0e79d13923005ee6abcb44395fff8ac500b7
Christopher Kullenberg <christopher.kullenberg@gmail.com>
2018-11-17 22:38:14 +0100

    some cleanup

--- flashbackscraper.py
+++ flashbackscraper.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Usage: 
-python fbscraper.py <link to thread url>
+python flashbackscraper.py <link to thread url>
 Example url: https://www.flashback.org/t2975477
+Written by: Christopher Kullenberg <christopher.kullenberg@gu.se>
 """
 from bs4 import BeautifulSoup
 import requests
@@ -13,13 +14,9 @@ import sys
 import datetime
 import csv
 
-# starturl = "https://www.flashback.org/t2975477"
-counter = 1 # dont forget to reset this variable
-
+counter = 1
 
 def parsethread(nexturl):
-    '''Takes a Flashback url and extracts username, 
-    datetime, quotes and comment body'''
     print("Scraping", nexturl)
     threadnumber = nexturl[26:]
     postidlist = []
@@ -38,7 +35,8 @@ def parsethread(nexturl):
     heading = soup.findAll("div", class_="post-heading")
     print("Length: " + str(len(postbody)))
     for p in postbody:
-        postid = re.findall("(?<=id\=\"post\_message\_).*?(?=\"\>)", str(p), re.IGNORECASE)
+        postid = re.findall("(?<=id\=\"post\_message\_).*?(?=\"\>)", str(p), 
+                            re.IGNORECASE)
         if postid:
             postidlist.append(postid[0])
     for u in username:
@@ -47,11 +45,11 @@ def parsethread(nexturl):
         else:
             userlist.append(u.text)
     for h in heading:
-        #print(h.text)
         yesterday = datetime.date.today() - datetime.timedelta(1)
         todaymatch = re.findall("Idag,\s\d\d\:\d\d", h.text, re.IGNORECASE)
         yesterdaymatch = re.findall("Igår,\s\d\d\:\d\d", h.text, re.IGNORECASE)
-        match = re.findall("\d\d\d\d\-\d\d\-\d\d,\s\d\d\:\d\d", h.text, re.IGNORECASE)
+        match = re.findall("\d\d\d\d\-\d\d\-\d\d,\s\d\d\:\d\d", h.text, 
+                           re.IGNORECASE)
         if todaymatch:
             datelist.append(datetime.date.today())
             #print(datetime.date.today())
@@ -67,29 +65,24 @@ def parsethread(nexturl):
     for p in postbody:
         bodylist.append(p.text)
     for p in postbody:
-        match = re.findall("(?<=Ursprungligen postat av ).*", p.text, re.IGNORECASE)
+        match = re.findall("(?<=Ursprungligen postat av ).*", p.text, 
+                           re.IGNORECASE)
         if match:
             inreplylist.append(match[0])
         else:
             inreplylist.append("none")
 
-    #print(postidlist, userlist, datetimelist, bodylist, inreplylist)
-    print(len(postidlist), len(userlist), len(datelist), len(timelist), len(bodylist), len(inreplylist))
+    print(len(postidlist), len(userlist), len(datelist), len(timelist), 
+          len(bodylist), len(inreplylist))
     #print(soup)
     for n in range(0,12):
         print("Adding post", str(((counter * 12) + n) - 12), "to database")
-        #print(postidlist[n])
-        #print(userlist[n])
-        #print(datelist[n])
-        #print(timelist[n])
-        #print(bodylist[n])
-        #print(inreplylist[n])
-        #print("------------")
         try:
             cursor.execute('''
             INSERT INTO fb(idnumber, user, date, time, body, inreply)
             VALUES(?,?,?,?,?,?)''', 
-            (postidlist[n], userlist[n], datelist[n], timelist[n], bodylist[n], inreplylist[n])
+            (postidlist[n], userlist[n], datelist[n], timelist[n], 
+             bodylist[n], inreplylist[n])
             )
             db.commit()
         except (IndexError, sqlite3.IntegrityError) as e:
