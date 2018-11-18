c04032a8104ef893b3c53be2be0b6e45a42e7705
Christopher Kullenberg <christopher.kullenberg@gmail.com>
2018-11-18 13:25:54 +0100

    added .gexf export script

---
+++ sqlite2gexf.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import sqlite3
+import sys
+
+filename = sys.argv[1]
+
+db = sqlite3.connect(filename)
+cursor = db.cursor()
+
+selectedges = cursor.execute('SELECT user, inreply FROM fb')
+edges = cursor.fetchall()
+
+G = nx.DiGraph()
+for e in edges:
+    if e[1] == 'none':
+         continue
+    else:
+         print(e[0], e[1])
+         G.add_edge(e[0], e[1])
+
+outfilename = filename[:8] + ".gexf"
+print("\nSaving Gexf-file as", outfilename, "Have fun mapping networks!")
+nx.write_gexf(G, outfilename)
